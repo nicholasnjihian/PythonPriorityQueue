703e3d9018aea9f91e4d556d465ae9478827c929
Nicholas Njihia <njihianicklas55@gmail.com>
2020-11-30 00:15:36 +0300

    Edited PriorityQueue.py

--- PriorityQueue.py
+++ PriorityQueue.py
@@ -2,11 +2,10 @@ class Node:
     def __init__(self, data, next=None, previous=None, priority):
         self.data = data
         self.next = next
-        self.previous = previous
         self.priority = priority
         
     def __repr__(self):
-        return self.data
+        return f"{self.data}; -> {self.next} ; Priority == {self.priority}"
         
 class PriorityQueue:
     def __init__(self):
@@ -20,76 +19,104 @@ class PriorityQueue:
             return False
     
     def clear(self):
+        self.length = 0
+        self.head = None
         
        
     
     def insertInQueue(self, content, priority):
-        #Create a Node with content and priority values
+       #No node available(even head node none):
+       #New Node becomes self.head
+
         node = Node(content, priority)
         
-        #If there is no node, not even head, we initialize our head with the node we created above
         if self.head is None:
-            self.head = node
-            
-        #If there is at least one node
+            self.head = node 
         else:
+            #Head node present but no other node present
+            
+           
             #If there is no node other than head, i.e., head.next is None
-            if self.head.next is None:
-                #If the head node has a lower priority e.g. 5 than the created node's priority e.g 1,
-                #we need to move the head to node.next
-                if self.head.next.priority > node.priority:
-                    self.head.next = self.head
+            #Compare priority of our new node and the head node
+            #Scenario 1:  -> If new node has higher priority, self.head becomes our new node 
+            #and the original self.head is pushed to second position in the Queue.
+            #Scenario 2:  -> If new node has lower priority, it becomes second in Queue
+ 
+            adjacent_to_head = self.head.next
+            if adjacent_to_head is None:
+                #Scenario 1
+                if self.head.priority > node.priority: #Hence node has higher priority
+                    duplicate = self.head
                     self.head  = node
+                    self.head.next = duplicate
+                #Scenario 2
                 else:
-                    
-                    #If the new created node's priority is lower than the self.head's priority
-                    #push the new node to the end of the PriorityQueue
                     self.head.next = node
-                    self.head.next.previous = self.head
-                    
-            #If there are other nodes or there is another node, other than self.head
-            #we need to compare the priorities again
-            #If the newly created node's priority is higher(for instance a value such as 1),
-            #we need to push every other node with a lower priority to the back of the queue, 
-            #further back to give space to the new node.
-            #If the created node's priority is lower it will be moved further back up to where no other node 
-            #before it has a lower priority. So if we have a new node with priority 3 and a Queue with priorities
-            #[1,2,6,8], the Queue will become [1,2,3,6,8].
             else:
-                last = self.head
+                # Head node present and in addition one or more other nodes are present:
+                # Note: Every time we add a new node to the Queue, the Queue is ordered from 
+                # highest priority to lowest.
                 
-                #Loop for so long as the priority of the created node is higher(a lower value is higher)
-                # and push nodes with lower priority further bact the Queue
-                while last.priority > node.priority:
+                #Therefore,
+                #Scenario 1:->  Our new Node has higher priority, it automatically becomes self.head.
+                #Scenario 2: -> 
+                #Our new node has lower priority. We need to loop through the Queue to find the appropriate 
+                #position for the node given its priority.
+                #e.g. if we have a Queue = [1,2,5,7,9] and new node 6, the Queue needs to become [1,2,5,6,7,9].
+                
+                #Scenario 1:
+                if self.head.priority > node.priority: # if node has higher priority
+                    duplicate = self.head
+                    self.head = node
+                    self.head.next = duplicate
+                #Scenario 2:
+                elif self.head.priority < node.priority: # if node priority is lower than self.head priority
+                    last=self.head
+                    counter = 1 #We can store the position at which we find a lower priority(higher number) 
+                    #e.g 7 in the above example with Queue [1,2,5,7,9], so as to push all the other 
+                    #lower priority numbers further back the Queue
                     
-                    #loop to the end of the Queue
-                    #We then create a new node at the Queue's end 
-                    #and then copy the last existing node to this new node
                     while last.next:
-                        last=last.next
-                    last.next = last
-                    
-                    #Now loop back to the head shifting the nodes to the back
-                    #[self.head > Node1 > Node2 > Node3 > Node3] becomes
-                    #[new self.head > former self.head > Node1 > Node2 > Node3]
-                    first = last
-                    while first.previous:
-                        first=first.previous
+                        if counter == length:
+                            last.next = node
+                            break
+                        if last.priority > node.priority: #Test if new node has higher priority
+                            node.next=last
+                            break
+                        counter+=1
+                        last=last.next  
                         
-                        
-                    
-                    
-                
+                    if counter != length:
+                        idx_at_which_to_append = counter - 1
+                        last=self.head
+                        counter = 1
+                        while last.next:
+                            if counter == idx_at_which_to_append:
+                                last.next = node
+                else: #self.head.priority is equal to node.priority
+                    val = self.head
+                    counter = 1
+                    while val.next:
+                        if val.priority != node.priority:
+                            node.next = val
+                            break
+                        counter += 1
+                        val = val.next
+                    val = self.head
+                    idx_at_which_to_append = counter - 1
+                    counter = 1
+                    while val.next:
+                        if counter == idx_at_which_to_append:
+                            val.next = node
+                            break
+                        counter += 1
+                        val = val.next      
         self.length = self.length + 1
         
     
     def removeFromQueue(self):
-        
-    def __iter__(self):
-        node = self.head
-        while node is not None:
-            yield node
-            node = node.next_node
-            
-
+        content = self.head.data
+        self.head = self.head.next
+        self.length = self.length - 1
+        return content
 
